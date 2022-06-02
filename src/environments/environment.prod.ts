d1bb805436ec251393fbb83b9f99fe700608ec76
pirumpi <pirumpi@gmail.com>
2022-06-02 16:59:30 -0600

    Fixing heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+const qs = new URLSearchParams(window.location.search);
 export const environment = {
-  production: true
+  production: true,
+  
+  cuss2Config: {
+    cussUrl: qs.get('url') || 'https://localhost:22222',
+    clientId: qs.get('id') || 'EL',
+    clientSecret:  qs.get('secret') || '12345678'
+  }
 };
