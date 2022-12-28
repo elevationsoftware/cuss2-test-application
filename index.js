11e0def1b11b330bd61089e82958f1e4ad754f1d
pirumpi <pirumpi@gmail.com>
2022-12-28 11:55:13 -0700

    Fixing libs ref

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-// import { express } from 'express'
-// import { bodyParser } from 'body-parser'
-// import { compression } from 'compression'
-//
-// import { path } from 'path';
-// import { fs } from 'fs';
-// const app = express();
-// import { http } from 'http'
-// http.Server(app);
-// const port = process.env.PORT || 3001;
-//
-// function setLongTermCache(res) {
-//   res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
-//   res.setHeader("Cache-Control", "public, max-age=2592000");
-// }
-//
-// app
-//   .use(compression())
-//   .use(bodyParser.json())
-//   .use(express.static(path.join(__dirname, 'dist', 'cuss2-example-angular'), {
-//     setHeaders(res, path) {
-//       if (path.match(/\.(js|css|png|jpg|jpeg|gif|ico|json|woff|woff2)$/)) {
-//         setLongTermCache(res);
-//       }
-//     }
-//   })
-//   )
-//   .use((req, res, next) => {
-//     if (path.extname(req.path).length > 0) {
-//       next();
-//     } else {
-//       res.write(fs.readFileSync(path.join(__dirname, 'dist', 'cuss2-example-angular', 'index.html')));
-//       res.end()
-//     }
-//   });
-//
-// http.listen(port);
-// console.info('[INFO] [${new Date().toLocaleString()}] Running on port: ' + port);
+const  express = require('express');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+
+function setLongTermCache(res) {
+  res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
+  res.setHeader("Cache-Control", "public, max-age=2592000");
+}
+
+app
+  .use(compression())
+  .use(bodyParser.json())
+  .use(express.static(path.join(__dirname, 'dist', 'cuss2-example-angular'), {
+    setHeaders(res, path) {
+      if (path.match(/\.(js|css|png|jpg|jpeg|gif|ico|json|woff|woff2)$/)) {
+        setLongTermCache(res);
+      }
+    }
+  })
+  )
+  .use((req, res, next) => {
+    if (path.extname(req.path).length > 0) {
+      next();
+    } else {
+      res.write(fs.readFileSync(path.join(__dirname, 'dist', 'cuss2-example-angular', 'index.html')));
+      res.end()
+    }
+  })
+  .listen(port);
+console.info('[INFO] [${new Date().toLocaleString()}] Running on port: ' + port);
